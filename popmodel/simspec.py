0820da4789b2dc9fd17084129fc573ada479f52c
Adam Birdsall <abirdsall@gmail.com>
2014-12-16 12:59:22 -0600

    simspec updates for voigt intercomparison

--- popmodel/simspec.py
+++ popmodel/simspec.py
@@ -23,7 +23,8 @@ def dornvoigt(wc, wd, wnum):
     Parameters
     ----------
     wc : float
-    Collision half width parameter for selected background gas and pressure, pm
+    Collision half width parameter (Lorentzian) for selected background gas and
+    pressure, pm
     wd : float
     Half width parameter for Doppler-broadened spectral line, pm
     wnum : float
@@ -36,35 +37,127 @@ def dornvoigt(wc, wd, wnum):
     F_norm : ndarray
     Voigt profile, normalized to integral of 1
     '''
-
-    wv = wc/2+np.sqrt(wc**2/4+wd**2) # approx of Whiting 1968, pm
-    width = 20. # pm
+    # work in wavelength rather than wavenumber
     wlength = 1e10/wnum # pm
-    xarr = np.linspace(wnum-width,wnum+width,1.e3) # pm
-    xstep = xarr[1]-xarr[0] # pm
-    # delta is 'normalized wavelength' to Voigt width wv
-    delta = (xarr - wnum)/wv # ratio of pm
-    # F1 + F2 is first approx given in Whiting et al. F3 is further refinement
-    # with better agreement -- has fractional powers of negative numbers,
-    # to avoid getting NaN, take absolute value of delta first
-    F1 = (1-wc/wv)*np.exp(-4*np.log(2)*delta**2)
-    F2 = (wc/wv)/(1+4*delta**2)
-    F3 = 0.016*(1.-wc/wv)*(wc/wv)\
-            *(np.exp(-.04*np.abs(delta)**2.25)-10/(10+np.abs(delta)**2.25))
-    F = F1+F2+F3
-    # normalize so that area under F is zero:
-    av = 1.065 + 0.447*wc/wv + 0.058*(wc/wv)**2 # lineshape factor, Dorn
-    norm_factor = 1/(wv*av) # see Dorn Eq 10
-    F_norm = F*norm_factor
-    return xarr,F_norm
 
-def simline(hitline,xarr=None):
-    '''Calculate simulated absorption spectrum for a single line.
+    # calculate voigt half width wv from wc and wd
+    wv = wc/2+np.sqrt(wc**2/4+wd**2) # approx of Whiting 1968, pm
+
+    # set up array of wavelengths to calculate profile over
+    width = 20. # pm
+    xarr = np.linspace(wlength-width,wlength+width,1.e3) # pm
+    xstep = xarr[1]-xarr[0] # pm
+
+    # delta is 'normalized wavelength' to Voigt width wv
+    delta = (xarr - wlength)/(wv*2) # ratio of pm
+
+    # calculate Voigt profile: terms are functions of delta, weighted by the
+    # ratio wc/wv. F1 + F2 is first approx given in Whiting et al. F3 is
+    # refinement with better agreement -- has fractional powers of negative
+    # numbers. To avoid getting NaN, take absolute value of delta first
+    F1 = (1-wc/wv) * np.exp(-4*np.log(2)*delta**2)
+    F2 = (wc/wv) / (1+4*delta**2)
+    F3 = 0.016*(1.-wc/wv)*(wc/wv)\
+            * (np.exp(-.04*np.abs(delta)**2.25)-10/(10+np.abs(delta)**2.25))
+    voigt = F1 + F2 + F3
+
+    # normalize so that area under F is zero:
+    av = 1.065 + 0.447*wc/wv + 0.058*(wc/wv)**2 # lineshape factor approx, Dorn
+    norm_factor = 1/(2*wv*av) # see Dorn Eq 10
+    voigt_norm = voigt*norm_factor
+    areaplotted = voigt_norm.sum() * xstep
+    if areaplotted < 0.9:
+        print 'Warning: profile contains <90% of total area:', areaplotted
+    return xarr,voigt_norm
+
+def voigt(xarr,amp,xcen,wc,wd,normalized):
+    """
+    *modified version for quality control purposes -- comparison to dornvoigt*
+
+    Normalized Voigt profile from pyspeckit, on Github.
+
+    z = (x+i*gam)/(sig*sqrt(2))
+    V(x,sig,gam) = Re(w(z))/(sig*sqrt(2*pi))
+
+    The area of V in this definition is 1.  If normalized=False, then you can
+    divide the integral of V by sigma*sqrt(2*pi) to get the area.
+
+    Original implementation converted from
+    http://mail.scipy.org/pipermail/scipy-user/2011-January/028327.html (had an
+    incorrect normalization and strange treatment of the input parameters)
+
+    Modified implementation taken from wikipedia, using the definition.
+    http://en.wikipedia.org/wiki/Voigt_profile
+
+    Parameters
+    ----------
+    xarr : np.ndarray
+    The X values over which to compute the Voigt profile, pm
+    amp : float
+    Amplitude of the voigt profile
+    if normalized = True, amp is the AREA
+    xcen : float
+    The X-offset of the profile
+    wc : float
+    Collision half width parameter (Lorentzian) for selected background gas and
+    pressure, pm
+    wd : float
+    Half width parameter for Doppler-broadened spectral line, pm
+    normalized : bool
+    Determines whether "amp" refers to the area or the peak of the voigt
+    profile
+
+    Outputs
+    -------
+    V : np.ndarray
+    Voigt profile y values for xarr, either normalized or not.
+    """
+    # calculate gamma, HWHM of collisional Lorentzian
+    gamma = wc # pm
+    # calculate sigma, std dev of Doppler Gaussian, from Doppler HWHM, wd
+    sigma = wd / np.sqrt(2*np.log(2)) # Wikipedia, Voigt profile, pm
+    # z is argument passed to Faddeeva function
+    z = ((xarr-xcen) + 1j*gamma) / (sigma * np.sqrt(2))
+    # voigt profile is real part of Faddeeva function, wofz() in scipy
+    V = amp * np.real(scipy.special.wofz(z))
+    if normalized:
+        return V / (sigma*np.sqrt(2*np.pi))
+    else:
+        return V
+
+def simline(hitline,xarr=None,press=oh.op_press,T=oh.temp):
+    '''Calculate simulated absorption spectrum for a single HITRAN line.
     
     Follow treatment in Dorn et al., J Geophys Res 100 (D4), 7397-7409, 1995.
     Represent absorption cross section spectrum as a product of (1) the total
     integrated absorption cross-section sigma_tot, (2) relative population
-    density popdens, and (3) voigt lineshape.
+    density popdens, and (3) voigt lineshape. To generate Voigt lineshape, use
+    scipy wofz function as implemented by pyspeckit, instead of Dorn et al.'s
+    use of older literature approximation of Whiting et al., J Quant Spectrosc
+    Radiat Transfer 8, 1379-1384, 1968. (Checked two methods for
+    near-consistency.)
+
+    Parameters
+    ----------
+    hitline : ndarray
+    1D recarray in format of a single line of output from
+    loadHITRAN.processHITRAN.
+    xarr : ndarray (optional)
+    1D array of frequency values to calculate spectrum over, Hz. Default is
+    None, and if no xarr is given, function creates one centered on the line
+    with a fixed default width and resolution.
+    press : float (optional)
+    Pressure, torr. Default to value in ohcalcs.
+    T : float (optional)
+    Temperature, K. Default to value in ohcalcs.
+
+    Outputs
+    -------
+    xarr : ndarray
+    1D array of frequency values used to create spectrum, Hz. Only really
+    helpful if xarr isn't passed to the function to begin wtih.
+    sigma_eff : ndarray
+    Effective absorption cross section values over xarr, cm^2.
     '''
     # extract values from hitline
     E_low = hitline['E_low']*1.986455684e-23 # lower-state E, J (from cm^-1)
@@ -73,10 +166,9 @@ def simline(hitline,xarr=None):
     Ja = hitline['Ja'] # total angular momentum, lower state
     Jb = hitline['Jb'] # total angular momentum, upper state
     Aba = hitline['Aba'] # s^-1
-    # other parameters
+
+    # convert wnum to frequency
     freq = wnum*c*100 # Hz
-    T = 296 # K
-    press = oh.op_press
 
     # (1) Calculate lineshape, in Hz domain, area normalized to 1:
     # Gaussian std dev for Doppler
@@ -111,17 +203,34 @@ def simline(hitline,xarr=None):
     # effective cross-section depends on population in lower state,
     # 'total' integrated cross-section, and lineshape (Voigt)
     sigma_eff = popdens * sigma_tot * lineshape # Dorn et al, Eq 7
-    print "popdens, sigma_tot, lineshape \
-    max:",popdens,sigma_tot,np.max(lineshape)
     return xarr,sigma_eff
 
 def simspec(hitlines):
-    '''combine set of hitlines into a spectrum'''
+    '''combine set of hitlines into a spectrum
+    
+    Calls simline function for each 'line' entry in input, passing an 'xarr' of
+    frequency values to cover all the lines. Dreadfully inefficient as stands
+    for a sparse spectrum, since it calculates and stores the full xarr range
+    for each line, most of which is essentially zero.
+    
+    Parameters
+    ----------
+    hitlines : ndarray
+    recarray in the format that loadHITRAN.processHITRAN spits out
+
+    Outputs
+    -------
+    xarr : ndarray
+    1D array of frequency values the spectrum was calculated over, Hz
+    sigma_eff_array
+    2D array of effective absorption cross-section values across xarr for each
+    line in hitlines, cm^2
+    '''
     # make unified x values to cover range around all center frequencies
     width = 400e6
     mincenterfreq = np.min(hitlines['wnum_ab'])*c*100 # Hz
     maxcenterfreq = np.max(hitlines['wnum_ab'])*c*100 # Hz
-    xarr = np.linspace(mincenterfreq-width, maxcenterfreq+width, 1.e3)
+    xarr = np.linspace(mincenterfreq-width, maxcenterfreq+width, 1.e6)
     # make ndarray for resulting absorprtion cross section spectra
     sigma_eff_array = np.empty([hitlines.size,xarr.size])
     for index, line in enumerate(hitlines):
